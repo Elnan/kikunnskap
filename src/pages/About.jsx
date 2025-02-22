685ed3926ed7094a036603db23ed8625dc1fbd14
olavelnan <olavelnan@gmail.com>
2025-02-22 14:30:46 +0100

    About - adjust CSS styles for improved layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,36 +10,36 @@ import { Icon } from "@dnb/eufemia";
 
 const About = () => {
   return (
-    <div class="about-wrapper">
-      <div class="text-section">
-        <h1 class="header">Finn pensjon du kan flytte til oss</h1>
-        <ul class="list">
+    <div className="about-wrapper">
+      <div className="text-section">
+        <h1 className="header">Finn pensjon du kan flytte til oss</h1>
+        <ul className="list">
           <li className="list-item">
             <Icon icon={checkIcon} size="auto" aria-hidden />{" "}
             <span className="list-item-text">Pensjonen forvaltes i fond</span>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <Icon icon={checkIcon} size="auto" aria-hidden />
             <span className="list-item-text"> Velg Pensjonsprofil</span>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <Icon icon={checkIcon} size="auto" aria-hidden />
             <span className="list-item-text">
               {" "}
               Full oversikt over pensjon og sparing
             </span>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <Icon icon={checkIcon} size="auto" aria-hidden />
             <span className="list-item-text"> Gratis pensjonsrådgivning</span>
           </li>
         </ul>
-        <div class="description">
+        <div className="description">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quae
           ipsa, delectus cupiditate, possimus necessitatibus error doloremque
           sequi quisquam architecto rem dolores. Voluptatem, totam velit.
         </div>
-        <div class="btn-section">
+        <div className="btn-section">
           <Button
             text="Les mer om pensjonskonto"
             on_click={() => {
@@ -59,7 +59,7 @@ const About = () => {
           />
         </div>
       </div>
-      <div class="img-container">
+      <div className="img-container">
         <img src={plantPic} alt="plant picture" />
       </div>
     </div>
